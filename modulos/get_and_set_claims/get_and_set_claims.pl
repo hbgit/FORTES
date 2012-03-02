bcbd071283089485fef88acc2205cdee91ffe42e
Herbert Oliveira <herberthb12@gmail.com>
2012-03-01 21:55:47 -0400

    Translated the comments in the get_and_set_claims.pl

--- modulos/get_and_set_claims/get_and_set_claims.pl
+++ modulos/get_and_set_claims/get_and_set_claims.pl
@@ -1,24 +1,22 @@
 #!/usr/bin/env perl
-
 #---------------------------------------------------------------
-# TO DO: pre-processamento de código
-# Doing: 
-# Objetivo: inserir as assertivas com as claims
-# Herbert O. Rocha
-#--------------------------------------------------------------- 
-
+# get_and_set_claims.pl module: Gathering claims in the file.csv
+# and insert the claims with assert in the code source.
+# Author: Herbert O. Rocha E-mail: herberthb12@gmail.com
+# Version: 2 - Year: 2012
+# License: GPL V3
+#---------------------------------------------------------------
 
 #dir resutl_claims
 $dir_result_claims = "result_claims/".$ARGV[0];
 
-#inicialização da flag do assert
+#Set flag to asserts
 $flag_assert = 0;
 
-#diretorio do código C analisado
-
+#Path where the source code it is
 $dir_c_code = $ARGV[1];
 
-#checking if is the claims to apply
+#checking if there is claims to apply
 $if_blank=`cat $dir_result_claims | wc -l`;
 if($if_blank == 0){
 	print 1;
@@ -26,102 +24,80 @@ if($if_blank == 0){
 }
 
 
-#lendo o arquivo com as informações abstraidas do resultado das claims
+#Reading the file.csv with gathered data from results of the claims
 open(ENTRADA , "<$dir_result_claims") or die "Nao foi possivel abrir o arquivo.tmp para leitura: $!";
 		
-while (<ENTRADA>) { # atribui à variável $_ uma linha de cada vez
+while (<ENTRADA>) { # assigning to the variable $_ one line per time
 	push(@LinhasFile_abs,$_);
 }
 
 close ENTRADA;
 
 
-# obtendo o nome do arquivo C
-# e lendo o código C original
+# Getting the name of the C file and reading the C source code
+#already pre-processing
 $name_c_code="";
 @linhas_c_code="";
 if($ARGV[0] =~ m/(^.[^.]*)/){
 	$name_c_code=$1;
-
-	#leitura do código C analisado	já pré-processado	
-	#PATH do código C analisado
-	#$path_c_code = $dir_c_code."/"."pre_".$name_c_code.".c";
-	$path_c_code = $dir_c_code;
-	#print $path_c_code."\n";
 	
+	#PATH of the C source code	
+	$path_c_code = $dir_c_code;
+		
 	open(C_CODE , "<$path_c_code") or die "Nao foi possivel abrir o arquivo.c para leitura: $!";
 		
-	while (<C_CODE>) { # atribui à variável $_ uma linha de cada vez
-		#verificando se já existe a biblioteca <assert.h>
+	while (<C_CODE>) { 
+		#checking if there is the library <assert.h>		
 		if($_ =~ m/(<assert.h>)/){
-			#flag demarcando que existe a biblioteca no código
+			#flag to set that already there is the library <assert.h>
 			$flag_assert = 1;
 		}
 		push(@linhas_c_code,$_);
 	}
-
-	close C_CODE;	 
-
-	
+	close C_CODE;	 	
 }	
 
-
-
-# criação do novo codigo C
-# e inserção das ASSERTS
+# Generating the new source C code, applying the CUnit model and insert the asserts
 $new_name_code_path = $ARGV[2];
 
-#abrindo o novo code C para escrever e inserir a assertiva
 #*** Open file ***
 open(NEW_FILEC , ">$new_name_code_path") or die "Nao foi possivel abrir o novo new_arquivo.c: $!";
 $size_new_file_inst = @linhas_c_code;
 
+# Reading the vector with the lines from file.abs that has the properties
+# gathering from claims
 
-# leitura do vetor com as linhas do arquivo ".abs" com
-# as propriedades abstraidas das CLAIMS
-
-#lista auxiliar utilizada para receber as Nº CLAIMS verificadas para inserção
-#no código
+# Auxiliary list adopting to receives the Nº CLAIMS checked that will be insert in the code
 @rec_ns_claims_verified=();
-#lista auxiliar Coments
+# Auxiliary list to comments
 @rec_claims_Coments_verified=();
-#lista aux property
+# Auxiliary list to property
 @rec_claims_properties_verified=();
-#lista aux line number of claim
+# Auxiliary list to line number of the claim
 @rec_claims_line_n_verified=();
 
 $size_line_file_abs = @LinhasFile_abs;
 
-for ($i=0; $i <= $size_line_file_abs; $i++) {
-	#print $LinhasFile_abs[$i]."-".$i."\n";
-	#@rec_each_line = split(/@/,$LinhasFile_abs[$i]);
+for ($i=0; $i <= $size_line_file_abs; $i++) {	
 	@rec_each_line = split(/;/,$LinhasFile_abs[$i]);
 	
-	#verificação caso exista diretivas para uso de expressões boolenas
-	#como as exibidas pelas "claims"
+	# Checking if there is directives to apply boolean expressions in the claims	
 	if($rec_each_line[-1] =~ /(.*FALSE.*|.*TRUE.*)/){
-		#verificando se faz parte do código		
-		if($1 =~ m/[^FALSE  TRUE]/){
-			#print $1."\n";
-			#$i++;
+		# checking is in the source code
+		if($1 =~ m/[^FALSE  TRUE]/){			
 			next;
-		}
-		#então a condição isolada de avaliação é das claims
-		else{
-			#$i++;
+		}else{
 			next;
 		}		
 	}
-	#verificando propriedades que "ainda" não são tratadas para a utilização
-	#direta nas assertivas
+	# Checking properties that "still" do not applied is this FORTES versions
+	# so jumping those	
 	elsif($rec_each_line[-1] =~ m/(!tmp\$.|SAME-OBJECT|INVALID-POINTER|POINTER_OFFSET)/){		
 		next;		
 	}
 	else{
-		#propriedades validadas para a criação das assertivas
-		#atribuindo as variaveis os valores correspondentes
-		#($num_line_claim,$num_claim,$coment_claim,$property_claim) = @rec_each_line;
-		
+		# valid properties to apply in assertion
+		# assigning to correct varibles or lists			
 		push(@rec_claims_line_n_verified,$rec_each_line[0]);
 		push(@rec_ns_claims_verified,$rec_each_line[1]);
 		push(@rec_claims_Coments_verified,$rec_each_line[2]);
@@ -132,70 +108,57 @@ for ($i=0; $i <= $size_line_file_abs; $i++) {
 	
 }
 
+# Handling source code to insert the assertions
+# Run the source code to count and to read the lines
 
-#manipulação do código para inserção das ASSERTS
-#percorrendo o código e contando as linhas do código
-
-#contador do somatorio para acompanhar o incremento das linhas
-#i=0 -> Sn=(i=0+1)+(i=1+1)+(i=2+1), sempre ,um incremento a mais na linha a ser manipulada
+# Counter for sum to control the lines increment
+#i=0 -> Sn=(i=0+1)+(i=1+1)+(i=2+1), always an increment more for analised line
 $sn_i=0;
 
-#print $rec_claims_line_n_verified[$cont]."\n";
-#verificando se a biblioteca do ASSERT existe
-#para inserir a biblioteca do assert
+#checking if there is the library <assert.h>		
 if($flag_assert != 1){	
 	#**********************************************************
 	# Includes for FORTES
 	#**********************************************************
 	print NEW_FILEC "#include \"CUnit/Basic.h\" //-> by FORTES \n";					
-	#agora já existe a assertiva
+	# Now already there is an assertion
 	$flag_assert = 1;
 	$sn_i=$sn_i+1;	
 }
 
-
-#contador para a lista de número de linhas
+# Counter to list de line number
 $cont_line=0;
 $flag_func_cunit=0;	
 for($cont=0;$cont<$size_new_file_inst; $cont++){				
 			
-	#verifica se a linha foi identificada na analise com ESBMC
-	#e consta no arquivo ".abs"
-	#o menos 1 no ($num_line_claim-1) é devido ao vetor iniciar em 0
-	#ou seja se a linhas for 1 no vetor será a posição do contador é 0
-	#logo 1-1=0
-	#elsif($cont == ($rec_claims_line_n_verified[$cont]-1)+$sn_i){
-	#print $cont."-".$rec_claims_line_n_verified[$cont_line]."\n";		
+	# Checking if the line was identified in the ESBMC verification and
+	# is in the ".abs" file
+	# the less 1 in ($num_line_claim-1) is beacause the vector to start in 0
+	# i.e., 1-1=0			
 	if($cont == ($rec_claims_line_n_verified[$cont_line]-1)+$sn_i){		
 				
-		#escreve a ASSERTIVA e os comentarios NO CÓDIGO
-		#remover \n
+		# writing an assertion and the comments in the source code		
 		chomp($rec_claims_properties_verified[$cont_line]);
 		chomp($rec_ns_claims_verified[$cont_line]);
 		
-		#<to_do> tratar comentários obter apenas o necessario
+		# <to_do> apply the resume comments
 		chomp($rec_claims_Coments_verified[$cont_line]);
 		
-		#remove espaços em branco
+		# remove the blank spaces
 		$rec_claims_properties_verified[$cont_line]=trim($rec_claims_properties_verified[$cont_line]);
-		#finalmente escreve no arquivo
-		print NEW_FILEC "CU_ASSERT($rec_claims_properties_verified[$cont_line]); //-> $rec_ns_claims_verified[$cont_line]::$rec_claims_line_n_verified[$cont_line]->by FORTES \n";		
-		#print $rec_claims_properties_verified[$cont_line];
 		
-		#testo para a proxima posição verificando se ela possue o 
-		#numero da linha
+		print NEW_FILEC "CU_ASSERT($rec_claims_properties_verified[$cont_line]); //-> $rec_ns_claims_verified[$cont_line]::$rec_claims_line_n_verified[$cont_line]->by FORTES \n";		
+				
 		$cont_line = $cont_line+1;
 		while($cont == ($rec_claims_line_n_verified[$cont_line]-1)+$sn_i){
-			#remover \n
+			
 			chomp($rec_claims_properties_verified[$cont_line]);
 			chomp($rec_ns_claims_verified[$cont_line]);
-		
-			#<to_do> tratar comentários obter apenas o necessario
+					
 			chomp($rec_claims_Coments_verified[$cont_line]);
-		
-			#remove espaços em branco
+					
 			$rec_claims_properties_verified[$cont_line]=trim($rec_claims_properties_verified[$cont_line]);
-			#finalmente escreve no arquivo		
+					
 			print NEW_FILEC "CU_ASSERT($rec_claims_properties_verified[$cont_line]); //-> $rec_ns_claims_verified[$cont_line]::$rec_claims_line_n_verified[$cont_line]->by FORTES \n";			
 			
 			$cont_line = $cont_line+1;			
@@ -208,7 +171,7 @@ for($cont=0;$cont<$size_new_file_inst; $cont++){
 	#**********************************************************
 	# Functions for CUnit with global variables - FORTES
 	#**********************************************************
-	#checking the flag, comments and includes after to insert the functions
+	#Checking the flag, comments and includes after to insert the functions
 	#Here I believe that code has at least one include in the header file
 	if($flag_func_cunit == 0 && !($linhas_c_code[$cont] =~ /(^\/|^#|^ |^\t|^$)/)){		
 		
@@ -314,8 +277,7 @@ int main(int argc, char *argv[3])
 #*** Close the file ***
 close(NEW_FILEC);
 
-#funções uteis
-#remove espaços em branco
+# The function that remove the blank spaces
 sub trim {
   my $string = shift;
   for ($string) {
