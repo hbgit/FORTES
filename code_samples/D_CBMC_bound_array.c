81e2da5001181ad31d8441c1769c17edf21c8f0e
Herbert Oliveira <herberthb12@gmail.com>
2012-03-02 14:12:28 -0400

    Update the code sample

--- code_samples/D_CBMC_bound_array.c
+++ code_samples/D_CBMC_bound_array.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int a[5], b[6];
-int main ( ){
-	int i, j, temp; 
-	a[0] = 1 ;
-	for(i=1; i<5; i++){
-		a[i]= a[i-1]+ i;
-		b[0] = 1;
+int main( ){
+	int i, j, temp; 	
+	for(i=0; i<5; i++){
+		a[i]= a[i+1]+ i;		
 		temp = a[i]*(i+1);
-		for(j=1; j<temp; j++){
+		for(j=0; j<temp; j++){
 			b[j]= b[i-1]+(temp*2);
 		}
 	}
